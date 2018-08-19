676a8352bc925d713316d38f36734cff48371691
kishida <naokikishida@gmail.com>
2018-08-19 23:01:25 +0900

    remove redundant code

--- app.js
+++ app.js
@@ -6,16 +6,7 @@ var camera = new THREE.PerspectiveCamera(
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//var controls = new THREE.VRControls(camera);
-//controls.standing = true;
-
-//var effect = new THREE.VREffect(renderer);
-//effect.setSize(window.innerWidth, window.innerHeight);
-
-//var manager = new WebVRManager(renderer, effect);
-
 window.addEventListener('resize', onResize, true);
-//window.addEventListener('vrdisplaypresentchange', onResize, true);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
@@ -31,12 +22,8 @@ document.body.appendChild( WEBVR.createButton( renderer ) );
 function render() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.001;
-  //requestAnimationFrame(render);
-
-  //controls.update();
 
   renderer.render(scene, camera);
-  //manager.render(scene, camera);
 }
 renderer.setAnimationLoop(render);
 render();
