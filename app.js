45747d0a4a62d5f4a5d19a61d0db6dc7a40af0b6
kishida <naokikishida@gmail.com>
2018-08-19 23:42:04 +0900

    add light

--- app.js
+++ app.js
@@ -9,10 +9,25 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 window.addEventListener('resize', onResize, true);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshLambertMaterial({color: 0x00ff00, overdraw:0.5});
 var cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 0, -5);
 scene.add(cube);
+
+light = new THREE.DirectionalLight(0xdfebff, 1);
+light.position.set(50, 200, 100);
+light.position.multiplyScalar(1.3);
+light.castShadow = true;
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+var d = 300;
+light.shadow.camera.left = -d;
+light.shadow.camera.right = d;
+light.shadow.camera.top = d;
+light.shadow.camera.bottom = -d;
+light.shadow.camera.far = 1000;
+scene.add(light);
+
 renderer.render(scene, camera);
 renderer.vr.enabled = true;
 
